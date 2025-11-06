df4dca1794a1b598af51454f69e3d31c3aa14bde
Asmin Pothula <axp4488@mavs.uta.edu>
2025-11-06 00:41:41 -0600

    fix5

--- api/hello.js
+++
@@ -1,6 +0,0 @@
-// api/hello.js
-export const config = { runtime: "nodejs" }; // <-- not nodejs20
-
-export default function handler(req, res) {
-  res.status(200).json({ ok: true, route: "/api/hello" });
-}
--- api/index.js
+++
@@ -1,6 +0,0 @@
-// api/index.js
-import serverless from "serverless-http";
-import app from "../backend/app.js";
-
-export const config = { runtime: "nodejs" };
-export default serverless(app);
